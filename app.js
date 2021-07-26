8e397d23f2584c7effc25eb87e626c08f8197222
Mehedi Hassan <howlader.mehedihassan@gmail.com>
2021-07-26 22:48:24 +0600

    Home page modified for ipad, responive menu bar added for mobile and about section completed

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
 const navbar = document.querySelector("nav");
 const height_of_navbar = navbar.getBoundingClientRect();
-console.log(window.pageYOffset, height_of_navbar.height);
+
+let flag_of_menu = true;
 
 window.addEventListener("scroll", () => {
-    if (window.pageYOffset > height_of_navbar.height) {
+    if (window.pageYOffset > height_of_navbar.height && flag_of_menu) {
         navbar.style.backgroundColor = "transparent";
     }
-    if (window.pageYOffset <= height_of_navbar.height) {
+    if (window.pageYOffset <= height_of_navbar.height && flag_of_menu) {
         navbar.style.backgroundColor = "#252525";
     }
+});
+
+const menu = document.querySelector(".menu");
+const open = document.querySelector(".open");
+const close = document.querySelector(".close");
+
+open.addEventListener("click", () => {
+    menu.style.display = "flex";
+    open.style.display = "none";
+    close.style.display = "block";
+    navbar.style.backgroundColor = "#252525";
+    flag_of_menu = false;
+});
+
+close.addEventListener("click", () => {
+    menu.style.display = "none";
+    close.style.display = "none";
+    open.style.display = "block";
+    navbar.style.backgroundColor = "transparent";
+    flag_of_menu = true;
 });
